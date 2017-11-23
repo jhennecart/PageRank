4fed549d9c84981b3924509ab0681becc782d346
jhennecart <32271337+jhennecart@users.noreply.github.com>
2017-11-23 16:36:24 +0100

    Correct power method
    +Test on the result of the file
    +Correction of power method (bug iteration and using norm)
    +Display of steps

--- pagerank.py
+++ pagerank.py
@@ -9,46 +9,63 @@ def main(argv):
 	path_file = argv[0] # Récupère le chemin du fichier placé en argument
 	A = np.loadtxt(path_file, delimiter=",", dtype=np.int) # Converti le fichier cs en une array
 	A = np.asmatrix(A) # Converti np.array en np.matrix
+
+	print("Matrice d'adjacence :")
+	print(A)
+
+	# Test si la matrice lue est carrée
+	if A.size != A.shape[0]**2:
+		print("ERROR : La matrice renseigné dans le fichier n'est pas carrée !")
+		return
 	xt = pageRankScore(A)
 	if xt is None:
-		print("Erreur dans le calcul")
-	else:
-		print(xt)
+		print("Erreur dans le calcul !")
+		return
+	print("Score PageRank final :")
+	print(xt)
 
 """
 Méthode implémentant la power method de façon de récursive jusqu'à ce que 
 la différence entre la valeur précédente et celle calculé soit insignifante, 
 en utilisant la précédente valeur du vecteur x et la matrice Google.
 """
-def powerMethod(G, xt):
+def powerMethod(G, xt, i=0):
 	xtNew = xt * G
-	if (xtNew == xt).all:
+	if i == 0 or i == 1 or i == 2:
+		print("Itération %d :"%(i + 1))
+		print(xtNew)
+	i = i + 1
+	if (np.abs(np.linalg.norm(xtNew, 1) - np.linalg.norm(xt, 1))) / np.linalg.norm(xt, 1) < 10**-8:
 		return xtNew
-	powerMethod(G,xtNew)
+	xtNew = powerMethod(G,xtNew,i)
+	return xtNew
 
 """
 Méthode calculant à partir de la matrice d'adjacence et d'un paramètre de téléportation
-la matrice de probabilité de transition et la matrice Google puis fait appelle à powerMethod
+la matrice de probabilités de transition et la matrice Google puis fait appelle à powerMethod
 afin de calculé le score pagerank.
 """
 def pageRankScore(A, alpha=0.9):
 	Aout = np.sum(A,axis=1) # Vecteur des degrés sortant
 	Ain = np.sum(A,axis=0) # Vecteur des degrés entrant
-	P = A / Aout[:] # Calcul de la matrice de probabilité de transition
+	print("Vecteur des degrés entrants :")
+	print(Ain)
+
+	# Calcul de la matrice de probabilités de transition
+	P = A / Aout[:]
+	print("Matrice de probabilités de transition :")
+	print(P) 
 
 	# Calcul de la matrice Google
-	G = np.zeros(A.shape)
-	for i in range(A.shape[0]):
-		for j in range(A.shape[0]):
-			G[i,j] = alpha * P[i,j] + (1 - alpha)*(1/A.shape[0])
+	G = alpha * P[:] + (1 - alpha) * (1/A.shape[0])
+	print("Matrice Google :")
+	print(G)
 
 	# Initialisation des scores par les degrés entrant
 	xt = Ain / np.sum(Ain)
 	# Appele de la power method
-	xt = powerMethod(G,xt)
-	# Test final pour savoir si tout c'est bien passé
-	if(np.sum(xt) != 1):
-		return None
+
+	xt = powerMethod(G,xt)	
 
 	return np.asarray(xt)
 
